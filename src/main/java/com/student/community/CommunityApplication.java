c224ce4cf75306cc44041628cda0d88c5615399f
Unbending-zyx <411707537@qq.com>
2020-02-02 16:19:02 +0800

    第一次提交

---
+++ src/main/java/com/student/community/CommunityApplication.java
@@ -0,0 +1,14 @@
+package com.student.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
